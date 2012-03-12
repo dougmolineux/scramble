7c7c68d4b98df8d9f38008f964ffe6681dea3ae5
Doug <doug.molineux@gmail.com>
2012-03-12 15:45:51 -0400

    Moved the hash function to a seperate file

---
+++ hash.c
@@ -0,0 +1,33 @@
+#include<openssl/md5.h>
+
+#define MAX_CHAR_SIZE 80
+
+// char *str2md5(const char *str[MAX_CHAR_SIZE], int length) {
+char *str2md5(char *str[MAX_CHAR_SIZE], int length) {
+
+	int n;
+	MD5_CTX c;
+	unsigned char digest[16];
+	char *out = (char*)malloc(33);
+
+	MD5_Init(&c);
+
+	while (length > 0) {
+        	if (length > 512) {
+            		MD5_Update(&c, str, 512);
+        	} else {
+            		MD5_Update(&c, str, length);
+        	}
+        	length -= 512;
+        	str += 512;
+    	}
+
+    	MD5_Final(digest, &c);
+
+    	for (n = 0; n < 16; ++n) {
+        	snprintf(&(out[n*2]), 16*2, "%02x", (unsigned int)digest[n]);
+    	}
+
+    	return out;
+}
+
